7601663cff9641c90e814b059a65255408159bc7
Emanueljuarez <zerauleuname@gmail.com>
2022-03-09 13:16:36 -0600

    Implementación de mapas e imagenes via url y iframe

--- src/components/Places/Create.js
+++ src/components/Places/Create.js
@@ -90,7 +90,7 @@ export default function Create(props){
             </form> */}
 
   
-<form style={{marginTop: 80}} onSubmit={ (e) => { handleSubmit(e) } } className="space-y-8 divide-y divide-gray-200 max-w-3xl mx-auto">
+<form style={{marginTop: 50, marginBottom: 90}} onSubmit={ (e) => { handleSubmit(e) } } className="space-y-8 divide-y divide-gray-200 max-w-3xl mx-auto">
   <div className="space-y-8 divide-y divide-gray-200">
     <div>
       <div>
@@ -105,6 +105,20 @@ export default function Create(props){
             
             <input type="text" name="name" id="name"  value={place.name}  onChange={(e) => { handleChange(e) }} autocomplete="name" className="flex-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300"/>
           </div>
+          
+          <div className="sm:col-span-3">
+          <label for="cathegory" class="block text-sm font-medium text-gray-700"> Category </label>
+          
+        </div>
+        <div className="mt-1">
+            <select id="cathegory" name="cathegory" onChange={handleChange}  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+              <option value="culture">culture</option>
+              <option value="outdoors">outdoors</option>
+              <option value="night_life">night life</option>
+              <option value="food">food</option>
+              <option value="other">other</option>
+            </select>
+          </div>
         </div>
 
         <div className="sm:col-span-6">
@@ -115,27 +129,8 @@ export default function Create(props){
           <p className="mt-2 text-sm text-gray-500">Write about this place so other users know what to expect.</p>
         </div>
 
-        <div className="sm:col-span-6">
-          <label for="cover-photo" className="block text-sm font-medium text-gray-700"> Add a photo </label>
-          <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-            <div className="space-y-1 text-center">
-              <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-              </svg>
-              <div className="flex text-sm text-gray-600">
-                <label for="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                  <span>Upload a file</span>
-                  <input id="file-upload" name="file-upload" type="file" className="sr-only"/>
-                </label>
-                <p className="pl-1">or drag and drop</p>
-              </div>
-              <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-            </div>
-          </div>
-        </div>
-
         <div className="sm:col-span-3">
-          <label for="photos" className="block text-sm font-medium text-gray-700"> Image </label>
+          <label for="photos" className="block text-sm font-medium text-gray-700"> Image url</label>
           <div className="mt-1">
             <input type="text" name="photos" id="photos" value={place.photos} onChange={(e) => { handleChange(e) }} autocomplete="given-name" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"/>
           </div>
@@ -150,7 +145,7 @@ export default function Create(props){
 
       
         
-          <div className="sm:col-span-3">
+          {/* <div className="sm:col-span-3">
           <label for="cathegory" class="block text-sm font-medium text-gray-700"> Category </label>
           <div className="mt-1">
             <select id="cathegory" name="cathegory" onChange={handleChange}  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
@@ -161,33 +156,20 @@ export default function Create(props){
               <option value="other">other</option>
             </select>
           </div>
-        </div>
+        </div> */}
 
-        </div>
-
-       
-
-        <div className="pt-8">
-      <div>
-        <h3 className="text-lg leading-6 font-medium text-gray-900">Place location.</h3>
-        <p className="mt-1 text-sm text-gray-500">Add the url of google maps.</p>
-      </div>
-      <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
         <div className="sm:col-span-3">
-          <label for="location" className="block text-sm font-medium text-gray-700"> URL </label>
+          <label for="location" className="block text-sm font-medium text-gray-700">Place location url</label>
+          
           <div className="mt-1">
             <input type="text" name="location" id="location" value={place.location} onChange={(e) => { handleChange(e) }} autocomplete="given-name" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"/>
           </div>
         </div>
-        
-      </div>
-     </div>
+
+        </div>
 
     </div>
 
-  
-
-    
     <div className="flex justify-center">
       <button type="submit" className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">create</button>
     </div>
--- src/components/Places/Details.js
+++ src/components/Places/Details.js
@@ -36,7 +36,7 @@ export default function Details(props){
 
     return(
         <>
-        <div>
+        <div >
             
 
             {!foundPlace && <h3>Place not found!</h3>} 
@@ -48,121 +48,104 @@ export default function Details(props){
                 <p>{foundPlace.cathegory}</p>
                 <p>{foundPlace.location}</p> */}
 
-                <div class="bg-white">
-  <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-x-8">
-    <div class="lg:max-w-lg lg:self-end">
+            <div class="bg-white">
+              <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-x-8">
+                <div className="lg:max-w-lg lg:self-end">
 
-      <nav aria-label="Breadcrumb">
-        <ol role="list" class="flex items-center space-x-2">
-          <li>
-            <div class="flex items-center text-sm">
-              <Link to="/places" class="font-medium text-gray-500 hover:text-gray-900">Back</Link>
-              {/* <a href="#" class="font-medium text-gray-500 hover:text-gray-900"> Back </a> */}
-            </div>
-          </li>
+                  <nav aria-label="Breadcrumb">
+                    <ol role="list" className="flex items-center space-x-2">
+                      <li>
+                        <div class="flex items-center text-sm">
+                          <Link to="/places" className="font-medium text-gray-500 hover:text-gray-900">Back</Link>
+                          {/* <a href="#" class="font-medium text-gray-500 hover:text-gray-900"> Back </a> */}
+                        </div>
+                      </li>
 
-         
-        </ol>
-      </nav>
+                    
+                    </ol>
+                  </nav>
 
-      <div class="mt-4">
-        <h1 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">{foundPlace.name} details</h1>
-      </div>
+                  <div className="mt-4">
+                    <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">{foundPlace.name} details</h1>
+                  </div>
 
-      <div class="mt-6 text-center">
-          <div class="mt-6 text-center">
-            <a href="" class="group inline-flex text-base font-medium">
-              
-              
-              <span class="text-gray-500 hover:text-gray-700">{foundPlace.cathegory}</span>
-            </a>
-          </div>
-      </div>
+                  <div className="mt-6 text-center">
+                      <div className="mt-6 text-center">
+                        <a href="" className="group inline-flex text-base font-medium">
+                          
+                          
+                          <span className="text-gray-500 hover:text-gray-700"> Category: {foundPlace.cathegory}</span>
+                        </a>
+                      </div>
+                  </div>
 
-      <section aria-labelledby="information-heading" class="mt-4">
-        <h2 id="information-heading" class="sr-only">Place information</h2>
+                  <section aria-labelledby="information-heading" className="mt-4">
+                    <h2 id="information-heading" class="sr-only">Place information</h2>
 
-        <div class="mt-4 space-y-6">
-          <p class="text-base text-gray-500">{foundPlace.description}</p>
-        </div>
+                    <div className="mt-4 space-y-6">
+                      <p className="text-base text-gray-500">{foundPlace.description}</p>
+                    </div>
+                    
+                    <div style={{marginTop: 40}} className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-1">
+                    <iframe src={foundPlace.location} width="570" height="450"  allowfullscreen="" loading="lazy"></iframe>
+                    </div>
 
-        
-      </section>
-    </div>
-
-    
-    <div class="mt-10 lg:mt-0 lg:col-start-2 lg:row-span-2 lg:self-center">
-      <div class="aspect-w-1 aspect-h-1 rounded-lg overflow-hidden">
-        <img src="https://tailwindui.com/img/ecommerce-images/product-page-04-featured-product-shot.jpg" alt="Model wearing light green backpack with black canvas straps and front zipper pouch." class="w-full h-full object-center object-cover"/>
-      </div>
-    </div>
-
-    
-    <div class="mt-10 lg:max-w-lg lg:col-start-1 lg:row-start-2 lg:self-start">
-      <section aria-labelledby="options-heading">
-        <h2 id="options-heading" class="sr-only">Product options</h2>
-
-        <form>
-          {/* <div class="sm:flex sm:justify-between">
-           
-            <fieldset>
-              <legend class="block text-sm font-medium text-gray-700">Size</legend>
-              <div class="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-2">
-               
-                <label class="relative block border border-gray-300 rounded-lg p-4 cursor-pointer focus:outline-none">
-                  <input type="radio" name="size-choice" value="18L" class="sr-only" aria-labelledby="size-choice-0-label" aria-describedby="size-choice-0-description"/>
-                  <p id="size-choice-0-label" class="text-base font-medium text-gray-900">18L</p>
-                  <p id="size-choice-0-description" class="mt-1 text-sm text-gray-500">Perfect for a reasonable amount of snacks.</p>
-                  
-                  <div class="absolute -inset-px rounded-lg border-2 pointer-events-none" aria-hidden="true"></div>
-                </label>
+                  </section>
+                </div>
 
                 
-                <label class="relative block border border-gray-300 rounded-lg p-4 cursor-pointer focus:outline-none">
-                  <input type="radio" name="size-choice" value="20L" class="sr-only" aria-labelledby="size-choice-1-label" aria-describedby="size-choice-1-description"/>
-                  <p id="size-choice-1-label" class="text-base font-medium text-gray-900">20L</p>
-                  <p id="size-choice-1-description" class="mt-1 text-sm text-gray-500">Enough room for a serious amount of snacks.</p>
-                  
-                  <div class="absolute -inset-px rounded-lg border-2 pointer-events-none" aria-hidden="true"></div>
-                </label>
+                <div className="mt-10 lg:mt-0 lg:col-start-2 lg:row-span-2 lg:self-center">
+                  <div className="aspect-w-1 aspect-h-1 rounded-lg overflow-hidden">
+                    <img src={foundPlace.photos} alt="Place photo" className="w-full h-full object-center object-cover"/>
+                  </div>
+                </div>
+
+                
+                <div className="mt-10 lg:max-w-lg lg:col-start-1 lg:row-start-2 lg:self-start">
+                  <section aria-labelledby="options-heading">
+                    <h2 id="options-heading" className="sr-only">Product options</h2>
+
+                    <form>
+                      {/* <div class="sm:flex sm:justify-between">
+                      
+                        <fieldset>
+                          <legend class="block text-sm font-medium text-gray-700">Size</legend>
+                          <div class="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-2">
+                          
+                            <label class="relative block border border-gray-300 rounded-lg p-4 cursor-pointer focus:outline-none">
+                              <input type="radio" name="size-choice" value="18L" class="sr-only" aria-labelledby="size-choice-0-label" aria-describedby="size-choice-0-description"/>
+                              <p id="size-choice-0-label" class="text-base font-medium text-gray-900">18L</p>
+                              <p id="size-choice-0-description" class="mt-1 text-sm text-gray-500">Perfect for a reasonable amount of snacks.</p>
+                              
+                              <div class="absolute -inset-px rounded-lg border-2 pointer-events-none" aria-hidden="true"></div>
+                            </label>
+
+                            
+                            <label class="relative block border border-gray-300 rounded-lg p-4 cursor-pointer focus:outline-none">
+                              <input type="radio" name="size-choice" value="20L" class="sr-only" aria-labelledby="size-choice-1-label" aria-describedby="size-choice-1-description"/>
+                              <p id="size-choice-1-label" class="text-base font-medium text-gray-900">20L</p>
+                              <p id="size-choice-1-description" class="mt-1 text-sm text-gray-500">Enough room for a serious amount of snacks.</p>
+                              
+                              <div class="absolute -inset-px rounded-lg border-2 pointer-events-none" aria-hidden="true"></div>
+                            </label>
+                          </div>
+                        </fieldset>
+                      </div> */}
+                      {authStatus &&
+                      <div style={{marginLeft: 60}} className="mt-10">
+                        <Link to={`/place/${foundPlace._id}/edit`} class="w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500">Edit</Link>
+                      </div>
+                      }
+                    
+                    </form>
+                  </section>
+                </div>
               </div>
-            </fieldset>
-          </div> */}
-         
-          <div class="mt-10">
-            <button type="submit" class="w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500">Edit</button>
-          </div>
-         
-        </form>
-      </section>
-    </div>
-    
-  </div>
-  {authStatus &&
-            
-            <Link to={`/place/${foundPlace._id}/edit`}>Edit</Link>
-        
-        }
-
-</div>
-
-
-{/* <center>
-  <iframe src={foundPlace.location} width="800" height="600" ></iframe>
-</center> */}
-
-                </>
-            )}
-
-            
-            
-        
-        </div>
-
-      
-
-
+            </div>
         </>
+        )}      
+        </div>
+      </>
     )
     
 }
